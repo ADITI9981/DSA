de534ba4a6956b7d4ac95532270142f62d0d71b0
Aditi Yadav <munmunyadav9981@gmail.com>
2025-02-16 21:45:37 +0530

    Add files via upload

---
+++ 6_while_loop.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n,i=10;
+    cin>>n;
+    while(i>=1)
+    {
+       cout<<n*i<<' ';
+       i--;
+    }
+    return 0;
+}
---
+++ 7_function.cpp
@@ -0,0 +1,38 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    vector<int> passedBy(int a, int &b) {
+        vector<int> result;
+        result.push_back(a+1);
+        result.push_back(b+2);
+        return result;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int a, b;
+        cin >> a >> b;
+        Solution obj;
+        vector<int> ans = obj.passedBy(a, b);
+        cout << ans[0] << " " << ans[1] << "\n";
+    
+cout << "~" << "\n";
+}
+    return 0;
+}
+
+// } Driver Code Ends
